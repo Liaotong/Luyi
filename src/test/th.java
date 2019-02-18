884d968cd1c5904dc7c33f4ee64cf71437b39b74
Terry <307123711@qq.com>
2019-02-18 11:12:11 +0800

    new

---
+++ src/test/th.java
@@ -0,0 +1,27 @@
+package test;
+
+public class th {
+
+}
+
+//class Fu{
+//	public int num = 10;
+//	public final int num2 =20;
+//}
+//
+//class Zi extends Fu{
+//	public void show() {
+//		num = 100;
+////		num2 = 200;
+//		System.out.println(num);
+//		System.out.println(num2);
+//	}
+//}
+//class ZiDemo{
+//	public static void main(String[] args) {
+//		Zi z = new Zi();
+//		z.show();
+//	}
+//	
+//}
+
